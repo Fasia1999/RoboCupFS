85dbada4906400ded43ec86f22901367d434a429
Fasia1999 <527180748@qq.com>
2019-10-02 18:03:11 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 robovizPath="/home/fasia/roboCup/RoboViz/bin/linux-amd64"
-serverAddress="114.212.84.40"
-loginUser="robocup"
-loginPassword="rinc"
+serverAddress="*.*.*.*"
+loginUser="*"
+loginPassword="*"
 
 #server-remote & moniter-local
 gnome-terminal --geometry=100x30 --tab -e "bash -c \"./openServer.expect $serverAddress $loginUser $loginPassword; exec bash\"" --tab --working-directory=$robovizPath -e "bash -c \"./roboviz.sh --serverHost=$serverAddress; exec bash\""
